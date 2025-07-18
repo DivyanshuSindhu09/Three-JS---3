9983969924de044c9dcfd11f36a2e380cdc40b2d
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-18 18:43:41 +0530

    done......

--- camera/src/main.js
+++ camera/src/main.js
@@ -1,5 +1,21 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls'
+
+const canvas = document.querySelector('.webgl')
+
+console.log(OrbitControls)
+// cursor
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+
+  cursor.x = (event.clientX / sizes.width) - 0.5
+  cursor.y =  (event.clientY / sizes.height) - 0.5
+})
 
 const scene = new THREE.Scene()
 
@@ -13,11 +29,30 @@ const sizes = {
   height : 600
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
+
+
+
+// const aspectRatio = sizes.width/ sizes.height
+// const camera = new THREE.OrthographicCamera(-1*aspectRatio,1*aspectRatio,-1,1,0.1,100)
+// left, right, top, bottom, near and far
+
+
+
+
+// near and far parameters 
+// inke beech wala object hi dikhega camere mein
+// don't use extreme values
 scene.add(camera)
 camera.position.z = 3
+// camera.position.x = 2
+// camera.position.y = 2
 
-const canvas = document.querySelector('.webgl')
+
+
+const controls = new OrbitControls(camera, canvas)   
+controls.enableDamping = true 
+// smooth krne k liye damping -> update controls
 
 const renderer = new THREE.WebGLRenderer({
   canvas
@@ -29,10 +64,26 @@ const clock = new THREE.Clock()
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
   // console.log('first')
-  mesh.rotation.y = elapsedTime
-  mesh.rotation.x = elapsedTime
-  mesh.position.x = Math.sin(elapsedTime)
-  mesh.position.y = Math.cos(elapsedTime)
+
+  controls.update()
+  // mesh.rotation.y = elapsedTime
+
+
+  // mesh.rotation.x = elapsedTime
+
+// updateCamera
+
+
+      // camera.position.x = Math.sin(cursor.x * Math.PI * 2) *3
+      // camera.position.z = Math.cos(cursor.x * Math.PI * 2) *3
+      // camera.position.y = cursor.y * 9
+
+// orbitControls
+
+
+  camera.lookAt(mesh.position)
+
+
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
 }
